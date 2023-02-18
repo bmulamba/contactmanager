122f4f9193635d5ec93aac26e9f44538c2abb3e9
bmulamba <badibangamulamba@gmail.com>
2023-02-18 23:45:24 +0200

    asse

--- src/App.js
+++ src/App.js
@@ -1,83 +1,25 @@
-import React, { Component, lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import Card from "./Components/Card/Card";
-import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Spinner from "./Components/Spinner/Spinner";
+import logo from './logo.svg';
+import './App.css';
 
-const ProductList = lazy(() => import("./Components/ProductList/ProductList"));
-const ProductDetail = lazy(() =>
-  import("./Components/ProductDetails/ProductDetail")
-);
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-      cart: [],
-      product: [],
-      productCount: 1,
-    };
-  }
-
-  incrementProduct = () => {
-    this.setState(({ productCount }) => ({ productCount: productCount + 1 }));
-  };
-
-  decrementProduct = () => {
-    this.setState(({ productCount }) => ({ productCount: productCount - 1 }));
-  };
-
-  addToCard = (product) => {
-    this.setState((state) => ({
-      cart: [...state.cart, product],
-    }));
-  };
-
-  componentDidMount() {
-    fetch("https://apifakeapi.herokuapp.com/data")
-      .then((res) => res.json())
-      .then((data) => this.setState({ products: data }));
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Navbar cart={this.state.cart} total={this.state.total} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <ProductList
-                  products={this.state.products}
-                  addToCard={this.addToCard}
-                  handleProductDetail={this.handleProductDetail}
-                  incrementProduct={this.incrementProduct}
-                  productCount={this.state.productCount}
-                  decrementProduct={this.decrementProduct}
-                />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/product/:id"
-            element={
-              <Suspense fallback={<Spinner />}>
-                <ProductDetail
-                  incrementProduct={this.incrementProduct}
-                  addToCard={this.addToCard}
-                  productCount={this.state.productCount}
-                  decrementProduct={this.decrementProduct}
-                />{" "}
-              </Suspense>
-            }
-          />
-          <Route path="/cart" element={<Card />} />
-        </Routes>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
